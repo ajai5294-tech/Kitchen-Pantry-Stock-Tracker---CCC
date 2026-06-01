ac5c08110e2153517107fb377a8081582353a6af
simonvec08 <simonvec08@gmail.com>
2026-06-01 10:57:07 -0700

    did some stuff with scanner

--- tracker.java
+++ tracker.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public static void main(String[] args){
     ArrayList<String> arr = new ArrayList<String>();
+    System.out.println("Add items: ");
     Scanner input = new Scanner(System.in);
-    while(){
-
+    while(input != null){
+        arr.add(input);
 
        
     }
