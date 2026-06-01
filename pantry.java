3a55e6eb69793beee0fb080e6fbdce54a9e23b15
simonvec08 <simonvec08@gmail.com>
2026-06-01 11:22:18 -0700

    more ideas to use as pantry class outline/one method

---
+++ pantry.java
@@ -0,0 +1,25 @@
+public class pantry{
+    private ArrayList<items> storage = new ArrayList<>();
+    public pantry()
+    {
+        ArrayList<items> storage = new ArrayList<>();
+    }
+    public String checkForLowStock()
+    {
+        int count = 0;
+        System.out.println("Low on: ")
+        for (int i = 0; i < storage.size(); ++ i)
+        {
+            if (storage.get(i).getCapacity() <= )//minimum reqiurment
+                System.out.println(storage.get(i).getBrandName());
+                else
+                {
+                    count ++;
+                }
+        }
+        if (count == storage.size())
+        {
+            System.out.println("Nothing");
+        }
+    }
+}
