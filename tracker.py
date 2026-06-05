1f1d55c7e66575582865850838b251287e61b9c0
simonvec08 <simonvec08@gmail.com>
2026-06-05 18:12:38 +0000

    dkjhvgejhr

--- tracker.py
+++ tracker.py
@@ -1,7 +1,7 @@
 items = [1,2,3,4,5,6]
 for i in range(len(items)):
     min = i
-    for j in items:
+    for j in range(len(items)):
         if items[i] > items[j]:
             min = j
     items[i] = items[min]
