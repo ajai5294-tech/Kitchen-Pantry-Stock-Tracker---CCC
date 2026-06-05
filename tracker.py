c8055060230d3a5bfefc1437d071ba374cc3bc63
simonvec08 <simonvec08@gmail.com>
2026-06-05 18:18:14 +0000

    efds

--- tracker.py
+++ tracker.py
@@ -1,8 +1,10 @@
 items = [1,2,3,4,5,6]
 for i in range(len(items)):
     min = i
-    for j in range(len(items)):
-        if items[i] > items[j]:
+    for j in range(i + 1, len(items)):
+        if items[j] < items[min]:
             min = j
+    x = items[i]
     items[i] = items[min]
+    items[min] = x
 print(items)
